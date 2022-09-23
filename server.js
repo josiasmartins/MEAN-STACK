c16ea638d514bc276e02a6d078dade0825751202
Caitano <josiasmartinscaitano@gmail.com>
2022-09-23 07:41:47 -0300

    feat: Parseamento da URL

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-var http = require('http')
-    ,app = require('./config/express')
-    db = require('./config/database');
+var http = require('http');
+// import { http } from 'http';
 
-http.createServer(app).listen(3000, function() {
-    console.log('Servidor estutando na porta: ' + this.address().port);
+http.createServer(function(req, res) {
+
+    if (req.url.startsWith('/index.html')) req.end('index.html');
+
+    if (req.url.startsWith('/estilos.css')) req.end('estilos.css');
+
+    // console.log('PAPAI');
+    var indece = req.url.indexOf('=');
+    var parametro = req.url.substring(indece+1);
+    res.end(parametro);
+
+}).listen(3000, function() {
+    console.log('NEH')
 });
-
