3b57b8b4124ad061dac3f4fb90ecdfc866345395
Caitano <josiasmartinscaitano@gmail.com>
2022-09-26 08:37:19 -0300

    feat: ativado o middleware static

--- config/express.js
+++ config/express.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 
 // configurações do express
+// middleare: são filtro aplicado nas requisições
+app.use(express.static('./public'));
 
 // exportar
 module.exports = app;
--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 var app = require('./config/express');
 
 http.createServer(app).listen(3000, function() {
-    console.log('NEH')
+    console.log('servidor iniciado')
 });
