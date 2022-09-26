9b05b9f1e0fa0a3b05bcd447f6d924039869c3ab
Caitano <josiasmartinscaitano@gmail.com>
2022-09-26 09:16:44 -0300

    feat: verbos do middleware

--- config/express.js
+++ config/express.js
@@ -5,5 +5,16 @@ var app = express();
 // middleare: são filtro aplicado nas requisições
 app.use(express.static('./public'));
 
+app.get('/v1/fotos', function(req, res) {
+
+    var fotos = [
+        {_id: 1, titulo: 'Leão', url: 'http://www.fundosanimais.com/Minis/leoes.jpg'},
+        {_id: 2, titulo: 'Leão 2', url: 'http://www.fundosanimais.com/Minis/leoes.jpg'}
+    ]
+
+    res.json(fotos);
+    console.log('Tentando obter as fotos');
+});
+
 // exportar
 module.exports = app;
