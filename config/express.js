092e1aaa3389f4dc5089e293b65d3897f9148015
Caitano <josiasmartinscaitano@gmail.com>
2022-09-26 08:32:13 -0300

    feat: configurando o expres

---
+++ config/express.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+
+// configurações do express
+
+// exportar
+module.exports = app;
--- server.js
+++ server.js
@@ -1,17 +1,6 @@
 var http = require('http');
-// import { http } from 'http';
+var app = require('./config/express');
 
-http.createServer(function(req, res) {
-
-    if (req.url.startsWith('/index.html')) req.end('index.html');
-
-    if (req.url.startsWith('/estilos.css')) req.end('estilos.css');
-
-    // console.log('PAPAI');
-    var indece = req.url.indexOf('=');
-    var parametro = req.url.substring(indece+1);
-    res.end(parametro);
-
-}).listen(3000, function() {
+http.createServer(app).listen(3000, function() {
     console.log('NEH')
 });
