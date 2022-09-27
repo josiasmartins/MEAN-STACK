cea8a97c05461a9cc4dd5dbde81e178414f3fbc7
Caitano <josiasmartinscaitano@gmail.com>
2022-09-27 20:15:27 -0300

    feat: Removendo fotos

---
+++ app/routes/foto.js
@@ -0,0 +1,23 @@
+module.exports = function(app) {
+    app.get('/v1/fotos', function(req, res) {
+
+        var fotos = [
+            {_id: 1, titulo: 'Leão', url: 'http://www.fundosanimais.com/Minis/leoes.jpg'},
+            {_id: 2, titulo: 'Leão 2', url: 'http://www.fundosanimais.com/Minis/leoes.jpg'}
+        ]
+    
+        res.json(fotos);
+        console.log('Tentando obter as fotos');
+    });
+    
+    app.get('/v1/grupos', function(req, res) {
+        
+        var grupos = [
+            { _id: 1, nome: 'esporte' },
+            { _id: 1, nome: 'esporte' },
+            { _id: 1, nome: 'esporte' }
+        ];
+    
+        res.json(grupos);
+    });
+}
--- config/express.js
+++ config/express.js
@@ -5,16 +5,8 @@ var app = express();
 // middleare: são filtro aplicado nas requisições
 app.use(express.static('./public'));
 
-app.get('/v1/fotos', function(req, res) {
-
-    var fotos = [
-        {_id: 1, titulo: 'Leão', url: 'http://www.fundosanimais.com/Minis/leoes.jpg'},
-        {_id: 2, titulo: 'Leão 2', url: 'http://www.fundosanimais.com/Minis/leoes.jpg'}
-    ]
-
-    res.json(fotos);
-    console.log('Tentando obter as fotos');
-});
+require('../app/routes/foto')(app);
+require('../app/routes/grupo')(app);
 
 // exportar
 module.exports = app;
