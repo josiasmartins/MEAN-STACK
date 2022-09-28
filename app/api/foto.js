c58454cb5f0b2d7fea5a2299941213cac66be0ef
Caitano <josiasmartinscaitano@gmail.com>
2022-09-28 13:48:41 -0300

    issue: resolved issue post

--- app/api/foto.js
+++ app/api/foto.js
@@ -2,6 +2,8 @@ const app = require('../../config/express');
 
 var api = {};
 
+var CONTADOR = 2;
+
 var fotos = [
     {_id: 1, titulo: 'Leão', url: 'https://i.ytimg.com/vi/Ml5rhdvCRrs/maxresdefault.jpg'},
     {_id: 2, titulo: 'Leão 2', url: 'https://http2.mlstatic.com/D_NQ_NP_756502-MLB43611446286_092020-O.jpg'}
@@ -33,11 +35,16 @@ api.removePorId = function(req, res) {
 
 };
 
-app.adiciona = function(req, res) {
+api.adiciona = function(req, res) {
+    var foto = req.body;
+    foto._id = ++CONTADOR;
+    fotos.push(foto);
+
+    res.json(foto);
 
     console.log(req.body);
-    req.body;
-
+    // req.body;
 };
+
 
 module.exports = api;
