0bc595e8e5a0fb2fbda8c4b357dda45d783b8b32
Caitano <josiasmartinscaitano@gmail.com>
2022-09-28 17:57:26 -0300

    feat: Desconectando o banco de dados

--- config/database.js
+++ config/database.js
@@ -1,7 +1,34 @@
-var mongoose = require('mongoose');
+module.exports = function(uri) {
 
-mongoose.connect('mongodb://localhost/alurapic');
+    var mongoose = require('mongoose');
+    
+    mongoose.connect('mongodb://'+uri);
+    
+    // mongoose.connection.on('connected', function() {
+    //     console.log('conectado ao banco de dados');
+    // });
+    
+    mongoose.connection.on('connected', function() {
+        console.log('conectado ao banco de dados');
+    });
+    
+    mongoose.connection.on('error', function(error) {
+        console.log('Erro na conexão: '+ error);
+    });
+    
+    mongoose.connection.on('disconnected', function() {
+        console.log('Desconectado do MongoDB');    
+    });
+    
+    // SIGINT: evento sinaliza o termino da aplicação
+    process.on('SIGINT', function() {
+    
+        mongoose.connection.close(function() {
+            console.log('Conexão fechada pelo termino da aplicação');
+            // exit: retorna o statusCode do processo;
+            process.exit(0);
+        });
+    
+    });
 
-mongoose.connection.on('connected', function() {
-    console.log('conectado ao banco de dados');
-});
+}
