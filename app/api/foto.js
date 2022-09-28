1231a8ccb1bd1cd23b005ce8603b0d7d1d5e6da6
Caitano <josiasmartinscaitano@gmail.com>
2022-09-28 18:49:49 -0300

    feat: Acessando o banco de dados

--- app/api/foto.js
+++ app/api/foto.js
@@ -1,63 +1,76 @@
 const app = require('../../config/express');
+var mongoose = require('mongoose');
 
 var api = {};
 
-var CONTADOR = 2;
+// var CONTADOR = 2;
 
-var fotos = [
-    {_id: 1, titulo: 'Leão', url: 'https://i.ytimg.com/vi/Ml5rhdvCRrs/maxresdefault.jpg'},
-    {_id: 2, titulo: 'Leão 2', url: 'https://http2.mlstatic.com/D_NQ_NP_756502-MLB43611446286_092020-O.jpg'}
-];
+// var fotos = [
+//     {_id: 1, titulo: 'Leão', url: 'https://i.ytimg.com/vi/Ml5rhdvCRrs/maxresdefault.jpg'},
+//     {_id: 2, titulo: 'Leão 2', url: 'https://http2.mlstatic.com/D_NQ_NP_756502-MLB43611446286_092020-O.jpg'}
+// ];
 
 api.lista = function(req, res) {
     
-    res.json(fotos);
-    console.log('Tentando obter as fotos');
+    // res.json(fotos);
+    // console.log('Tentando obter as fotos');
+    
+    var model = mongoose.model('Foto');
+
+    model
+        .find({})
+        .then(function(fotos) {
+            res.json(fotos);
+        }, function(error) {
+            console.log(error);
+            res.status(500).json(error);
+        });
+
 };
 
 api.buscaPorId = function(req, res) {
 
-    var foto = fotos.find(function(foto) {
-        return foto._id = req.params.id;
-    });
+    // var foto = fotos.find(function(foto) {
+    //     return foto._id = req.params.id;
+    // });
 
-    res.json(foto);
+    // res.json(foto);
 
 };
 
 api.removePorId = function(req, res) {
     
-    fotos = fotos.filter(function(foto) {
-        return foto._id != req.params.id;
-    });
+    // fotos = fotos.filter(function(foto) {
+    //     return foto._id != req.params.id;
+    // });
 
-    res.sendStatus(204);
+    // res.sendStatus(204);
 
 };
 
 api.adiciona = function(req, res) {
-    var foto = req.body;
-    foto._id = ++CONTADOR;
-    fotos.push(foto);
+    // var foto = req.body;
+    // foto._id = ++CONTADOR;
+    // fotos.push(foto);
 
-    res.json(foto);
+    // res.json(foto);
 
-    console.log(req.body);
+    // console.log(req.body);
     // req.body;
 };
 
 api.atualiza = function(req, res) {
 
-    var foto = req.body;
-    var fotoId = req.params.id;
+    // var foto = req.body;
+    // var fotoId = req.params.id;
 
-    var indece = fotos.findIndex(function(foto) {
-        return foto._id == fotoId;
-    });
+    // var indece = fotos.findIndex(function(foto) {
+    //     return foto._id == fotoId;
+    // });
 
-    fotos[indece] = foto;
+    // fotos[indece] = foto;
 
-    res.sendStatus(200);
+    // res.sendStatus(200);
 
 }
 
