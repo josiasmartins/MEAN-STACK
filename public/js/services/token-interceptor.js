7b7f90ca304362ffe1a7e706afb2136a98742902
Caitano <josiasmartinscaitano@gmail.com>
2022-10-01 14:06:36 -0300

    feat: Interceptadores

--- public/js/services/token-interceptor.js
+++ public/js/services/token-interceptor.js
@@ -1,9 +1,17 @@
 angular.module('alurapic')
-    .factory('tokenInterceptor', function() {
+    .factory('tokenInterceptor', function($window) {
 
         var interceptor = {};
 
-        
+        interceptor.response = function(response) {
+            console.log('Recebi resposta');
+            var token = response.headers('x-access-token');
+            if (token) {
+                $window.sessionStorage.token = token;
+                console.log('Token armazenado no navegador');
+            }
+            return response;
+        }
 
         return interceptor;
 
